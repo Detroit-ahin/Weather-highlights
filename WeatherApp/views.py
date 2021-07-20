78a57f6f257d6f14525c9a75e5babdcff584a15a
Detroit-ahin <ahinsubhradas1997@gmail.com>
2021-07-20 10:57:16 +0530

    second

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+# import json to load json data to python dictionary
 import urllib.request
+# urllib.request to make a request to api
 import json
+
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         city = request.POST.get('city')
+        #''' api key might be expired use your own api_keyplace api_key in place of appid ="your_api_key_here "  '''
+  
+        # source contain JSON data from API
         # print(city)
         api_url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=19271528f8b9badf220b6280c99116e8').read()
         api_url2 = json.loads(api_url)
+                # converting JSON data to a dictionary
+
 
         data = {
             "country": city,
@@ -17,7 +25,8 @@ def index(request):
             "weather_humidity":api_url2['main']['humidity'],
             "weather_icon": api_url2['weather'][0]['icon'],
         }
-        
+                # data for variable list_of_data
+
     else:
         city = None
         data = {
@@ -28,6 +37,7 @@ def index(request):
             "weather_humidity":None,
             "weather_icon": None,
         }
+        #showing none
     print(data['weather_icon'])
     return render(request, 'index.html', {"city": city, "data" :data})
     
